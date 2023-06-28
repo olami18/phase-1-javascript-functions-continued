a99429a145eb8668fa0f7548185b051eac8214fa
+ <+>
2023-06-28 09:06:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function saturdayFun() {
+    console.log("This Saturday I want to roller-skate")
+      
+}
+function mondayWork() {
+    console.log("This Monday, I will go home")
+}
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,17 +1,3 @@
-describe("index.js", () => {
-  describe("defines saturdayFun function declaration as specified", function() {
-    it("function exists", function() {
-      expect(saturdayFun).to.exist
-    })
-
-    it("uses the default activity 'roller-skate' when no arguments are passed", function() {
-      expect(saturdayFun()).to.equal("This Saturday, I want to roller-skate!")
-    })
-
-    it("permits the default activity to be overriden", function() {
-      expect(saturdayFun("bathe my dog")).to.equal("This Saturday, I want to bathe my dog!")
-    })
-  })
 
   describe("defines mondayWork function expression as specified", function() {
     it("function exists", function() {
